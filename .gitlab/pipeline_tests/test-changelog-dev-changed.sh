8fd02dc7d9f01dfa7e9e9c979c99504d002cb146
Ryan Walder <ryan@tfu.io>
2022-10-27 12:04:00 +0100

    Update CI job 'changelog-has-changed'
    Target the destination branch for changelog-has-changed job rather than
    just checking against dev

--- .gitlab/pipeline_tests/test-changelog-dev-changed.sh
+++ .gitlab/pipeline_tests/test-changelog-dev-changed.sh
@@ -5,14 +5,19 @@ IFS=$'\n\t'
 # The following vars are set during the 'init' CI job.
 # CHANGELOG_FILE
 
+# If not during a MR related job this will not be set, so default to dev
+CI_MERGE_REQUEST_TARGET_BRANCH_NAME="${CI_MERGE_REQUEST_TARGET_BRANCH_NAME:-dev}"
+# Make a shorter variable
+BRANCH="${CI_MERGE_REQUEST_TARGET_BRANCH_NAME}"
+
 # Check that Gitlab has fetched the dev branch
-if ! git branch -a | grep -q 'remotes/origin/dev'; then
-  echo "remotes/origin/dev branch does not exist"
+if ! git branch -a | grep -q "remotes/origin/${BRANCH}"; then
+  echo "remotes/origin/${BRANCH} branch does not exist"
   exit 1
 fi
 
 # Test if the CHANGELOG has been updated
-if git diff --quiet HEAD remotes/origin/dev -- "${CHANGELOG_FILE}"; then
+if git diff --quiet HEAD "remotes/origin/${BRANCH}" -- "${CHANGELOG_FILE}"; then
   echo "❌ Changelog not changed"
   exit 1
 else
