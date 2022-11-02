93292340c9adf335a66f7682c58dfd6799015778
Ryan Walder <ryan@tfu.io>
2022-11-02 10:48:20 +0000

    Fix test-lang-file.sh to not silently fail

--- .gitlab/pipeline_tests/test-lang-file.sh
+++ .gitlab/pipeline_tests/test-lang-file.sh
@@ -5,19 +5,6 @@ IFS=$'\n\t'
 LANGFILE="src/lang/en.json"
 ERRORS=0
 
-# Check if a string is used in the system
-function check_string_exists() {
-  local strings
-  string="$1"
-
-  if ! grep -rq --exclude-dir=lang --exclude-dir=node_modules "${string}" ./*; then
-    echo "❌ String not used: ${string}"
-    ((ERRORS + 1))
-  else
-    echo "✅ ${string} used!"
-  fi
-}
-
 if [[ ! -f "${LANGFILE}" ]]; then
   echo "❌ Unable to find ${LANGFILE}"
   exit 1
@@ -38,16 +25,18 @@ else
 fi
 
 # Iterate through them and check if they exist elsewhere in the code
-# Background the check to do it in parallel
-for str in ${strings}; do
-  check_string_exists "${str}" &
+for string in ${strings}; do
+  if ! grep -rq --exclude-dir=lang --exclude-dir=node_modules "${string}" ./src; then
+    echo "❌ String not used: ${string}"
+    ((ERRORS = ERRORS + 1))
+  else
+    echo "✅ ${string} used!"
+  fi
 done
-# Wait for all background jobs to complete
-wait
 
 # If some do not exist elsewhere in the code fail this job
 if [[ "${ERRORS}" -gt 0 ]]; then
-  echo "❌ Some strings not detected, check the output above for more details."
+  echo "❌ ${ERRORS} strings not detected, check the output above for more details."
   exit 1
 else
   echo "✅ All good!"
