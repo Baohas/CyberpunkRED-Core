f4a51b3a247cf82220dcb225aef8add6c82d3142
dsorrent <Darin.Sorrentino@gmail.com>
2022-11-02 13:30:30 -0400

    Fix so we do not have to change the default setting every time.

--- src/modules/system/settings.js
+++ src/modules/system/settings.js
@@ -150,7 +150,7 @@ const registerSystemSettings = () => {
     scope: "world",
     config: true,
     type: String,
-    default: "4",
+    default: "newCprWorld",
     onChange: (value) => {
       LOGGER.log(`Changed dataModelVersion to ${value}`);
     },
