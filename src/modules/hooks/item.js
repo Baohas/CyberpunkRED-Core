a9ab4ef500e5e3d083d086e455d25d2710a672d6
jalensailin <jalen.michalslevy@gmail.com>
2022-11-03 19:18:50 -0400

    set activeNetRole automatically;

--- src/modules/hooks/item.js
+++ src/modules/hooks/item.js
@@ -71,6 +71,9 @@ const itemHooks = () => {
       if (doc.type === "role" && actor.system.roleInfo.activeRole === "") {
         actor.update({ "data.roleInfo.activeRole": doc.name });
       }
+      if (doc.type === "role" && !actor.itemTypes.role.some((r) => r.id === actor.system.roleInfo.activeNetRole)) {
+        actor.update({ "data.roleInfo.activeNetRole": doc.id });
+      }
       // when a new item is created (dragged) on a mook sheet, perform a couple changes like auto-equip
       if (Object.values(actor.apps).some((app) => app instanceof CPRMookActorSheet) && userId === game.user._id) {
         LOGGER.debug("handling a dragged item to the mook sheet");
