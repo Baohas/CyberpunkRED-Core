d873600512039d06d5c8716e7bf44b5045fe4244
jalensailin <jalen.michalslevy@gmail.com>
2022-11-10 13:08:56 -0500

    bugfix

--- src/modules/actor/sheet/cpr-actor-sheet.js
+++ src/modules/actor/sheet/cpr-actor-sheet.js
@@ -1157,7 +1157,12 @@ export default class CPRActorSheet extends ActorSheet {
     cprNewItemData.amount = formData.splitAmount;
     delete cprNewItemData._id;
     await this.actor.updateEmbeddedDocuments("Item", [{ _id: item.id, "system.amount": newAmount }]);
-    await this.actor.createEmbeddedDocuments("Item", [{ name: item.name, type: item.type, system: cprNewItemData }], { CPRsplitStack: true });
+    await this.actor.createEmbeddedDocuments("Item", [{
+      name: item.name,
+      type: item.type,
+      img: item.img,
+      system: cprNewItemData,
+    }], { CPRsplitStack: true });
   }
 
   /**
