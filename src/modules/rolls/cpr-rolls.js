d6660b6fe5eba4cc74b685eb26dd658153dd101c
dsorrent <Darin.Sorrentino@gmail.com>
2022-11-12 22:54:49 -0500

    Fix for Custom Skills showing the name as they were created. (ex: Basic Skill)

--- src/modules/rolls/cpr-rolls.js
+++ src/modules/rolls/cpr-rolls.js
@@ -346,7 +346,8 @@ export class CPRSkillRoll extends CPRStatRoll {
     this.skillValue = skillValue;
     this.roleName = roleName;
     this.roleValue = roleValue;
-    this.rollTitle = SystemUtils.Localize(`CPR.global.itemType.skill.${skillName.toLowerCase()}`);
+    this.rollTitle = SystemUtils.Localize(`CPR.global.itemType.skill.${SystemUtils.slugify(skillName)}`) === `CPR.global.itemType.skill.${SystemUtils.slugify(skillName)}`
+      ? skillName : SystemUtils.Localize(`CPR.global.itemType.skill.${SystemUtils.slugify(skillName)}`);
     this.universalBonusAttack = universalBonusAttack;
     this.rollPrompt = `systems/${game.system.id}/templates/dialog/rolls/cpr-verify-roll-skill-prompt.hbs`;
     this.rollCard = `systems/${game.system.id}/templates/chat/cpr-skill-rollcard.hbs`;
