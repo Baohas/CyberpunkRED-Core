32495d128372a1553e811b6a05a50cd69819fd8f
Ryan Walder <ryan@tfu.io>
2022-11-14 22:02:06 +0000

    Add gulp build/watch target for system languages
    * Check if strings in `en.json` exist in other language files then add
      them if not
    * Check if strings in other language files exist in `en.json` then
      delete if not

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -4,9 +4,9 @@ import * as bld from "./gulp/build.mjs";
 import * as packs from "./gulp/packs.mjs";
 
 export const clean = gulp.series(bld.cleanDist);
-export const assets = gulp.series(bld.copyAssets);
 export const system = gulp.series(bld.buildManifest);
 export const less = gulp.series(bld.compileLess);
+export const assets = gulp.series(bld.propagateLangs, bld.copyAssets);
 export const extractPacks = gulp.series(packs.extPacks);
 export const generatePacks = gulp.series(packs.genPacks);
 
