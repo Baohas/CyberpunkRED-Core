97b2022ae07f9bc2e605ebf3c8e04d896c9d36cb
Ryan Walder <ryan@tfu.io>
2022-11-28 13:06:18 +0000

    Add SVG minimization to build

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -10,14 +10,17 @@ export const assets = gulp.series(bld.propagateLangs, bld.copyAssets);
 export const extractPacks = gulp.series(packs.extPacks);
 export const generatePacks = gulp.series(packs.genPacks);
 export const generateChangelog = gulp.series(bld.buildChangelog);
+export const images = gulp.series(bld.processImages, bld.processSvgs);
 
 export const build = gulp.series(
   clean,
   assets,
   system,
   less,
+  images,
   generatePacks,
   generateChangelog,
+
 );
 
 // Don't just call `build` & `bld.watch` because `build` cleans the directory
@@ -27,6 +30,7 @@ export const watch = gulp.series(
   assets,
   system,
   less,
+  images,
   generateChangelog,
   bld.watchSrc,
 );
