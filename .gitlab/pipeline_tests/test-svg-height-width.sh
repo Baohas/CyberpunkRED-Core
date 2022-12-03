34042b482dfb091b37503b77f37c7b78ab6d1c29
dsorrent <Darin.Sorrentino@gmail.com>
2022-12-02 21:12:53 -0500

    Create pipeline test to check for height and width in the svg tag of images and fix all images that were missing them.

---
+++ .gitlab/pipeline_tests/test-svg-height-width.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ERRORS=0
+# Check SVG_LOCATION exits
+SVG_LOCATION="src/icons"
+
+# Check the SVG_LOCATION exists
+if [[ ! -d "${SVG_LOCATION}" ]]; then
+  echo "❌ Unable to find ${SVG_LOCATION}"
+  exit 1
+fi
+
+# Check we have files in SVG_LOCATION
+ALL_FILES=$(find "${SVG_LOCATION}" -type f -name "*.svg" -print | sort -u)
+
+if [[ -z "${ALL_FILES}" ]]; then
+  echo "❌ Unable to find any SVG files in ${SVG_LOCATION}"
+  exit 1
+fi
+
+for file in ${ALL_FILES}; do
+  SVG_HEADER=$(awk '/<svg/,/>/' ${file})
+  if [[ -z "$(echo ${SVG_HEADER} | grep ' height=')" || -z "$(echo ${SVG_HEADER} | grep ' width=')" ]]; then
+      echo "❌ 'height=""' and/or 'width=""' missing in the svg tag in ${file}"
+      ((ERRORS = ERRORS + 1))
+  fi
+done
+
+# If some svg tags are missing height/width fail this job
+if [[ "${ERRORS}" -gt 0 ]]; then
+  echo "❌ There are ${ERRORS} files missing height and/or width from the svg header tag, as listed above."
+  echo "Please add or correct the svg header tags."
+  exit 1
+else
+  echo "🎉 All good!"
+fi
