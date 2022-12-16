ded0215d567d2f33c0affc59c8165b5499930578
dsorrent <Darin.Sorrentino@gmail.com>
2022-12-16 12:08:37 -0500

    Re-wrote getAllUpgradesFor as part of this MR and missed one place where it was being called the old way.

--- src/modules/item/mixins/cpr-attackable.js
+++ src/modules/item/mixins/cpr-attackable.js
@@ -166,18 +166,17 @@ const Attackable = function Attackable() {
     cprRoll.addMod(actor.getArmorPenaltyMods(statName));
     cprRoll.addMod(actor.getWoundStateMods());
     cprRoll.addMod(skillMod);
-    const upgradeValue = this.getAllUpgradesFor("attackmod");
-    const upgradeType = this.getUpgradeTypeFor("attackmod");
+    const upgradeData = this.getAllUpgradesFor("attackmod");
     let upgradeResult = cprWeaponData.attackmod;
-    if (upgradeValue !== "" && upgradeValue !== 0) {
-      if (upgradeType === "override") {
-        upgradeResult = upgradeValue;
-      } else if (typeof upgradeResult !== "number" || typeof upgradeValue !== "number") {
-        if (upgradeValue !== 0 && upgradeValue !== "") {
-          upgradeResult = `${upgradeResult} + ${upgradeValue}`;
+    if (upgradeData.value !== "" && upgradeData.value !== 0) {
+      if (upgradeData.type === "override") {
+        upgradeResult = upgradeData.value;
+      } else if (typeof upgradeResult !== "number" || typeof upgradeData.value !== "number") {
+        if (upgradeData.value !== 0 && upgradeData.value !== "") {
+          upgradeResult = `${upgradeResult} + ${upgradeData.value}`;
         }
       } else {
-        upgradeResult += upgradeValue;
+        upgradeResult += upgradeData.value;
       }
     }
     cprRoll.addMod(upgradeResult);
