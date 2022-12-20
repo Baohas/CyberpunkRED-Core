c84890af50f4a88adb1daed2b0e929526dc75e2d
Ryan Walder <ryan@tfu.io>
2022-12-20 13:35:47 +0000

    Add validate-packs CI job

---
+++ .gitlab/pipeline_tests/test-packs.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+ERRORS=0
+SYSTEM_FILE="${SYSTEM_FILE:-src/system.json}"
+TEMPLATE_FILE="${TEMPLATE_FILE:-src/template.json}"
+
+# Get a list of itemTypes from the template.json file
+# Remove 'netarch' from the from the results as we don't validate those
+mapfile -t ITEMS < <(
+  jq \
+    --compact-output \
+    --raw-output \
+    '.Item.types | del(.[index("netarch")]) | .[]' \
+    "${TEMPLATE_FILE}"
+)
+
+# For each itemType run the YAML fragments through v8r
+for item in "${ITEMS[@]}"; do
+  # Shortcut v84 as we're doing error handling based on the output
+  results=$(npx v8r src/packs/**/"${item}".*.yaml 2>/dev/null || true)
+
+  # Parse out errors from the results
+  all_errors=$(
+    echo "${results}" |
+      jq 'del(.results[]
+      | select(.code == 0))
+      | [.results[]
+      | {file: .fileLocation, errors: .errors, code: .code}]'
+  )
+
+  # Get a count of the errors
+  end=$(echo "${all_errors}" | jq length)
+
+  count=0
+  while [[ "${count}" -lt "${end}" ]]; do
+    # Check the exit code of the test, and output the fielename, and error
+    # https://github.com/chris48s/v8r#exit-codes
+    code=$(echo "${all_errors}" | jq -r .["${count}"].code)
+    echo "❌ Errors detected in $(echo "${all_errors}" | jq -r .["${count}"].file)"
+    if [[ "${code}" -eq 99 ]]; then
+      echo "${all_errors}" | jq -r '.['"${count}"'].errors | .[]'
+    else
+      echo "Could not find schema for this file."
+    fi
+    ((ERRORS = ERRORS + 1))
+    ((count = count + 1))
+  done
+done
+
+if [[ "${ERRORS}" -gt 0 ]]; then
+  echo "❌ ${ERRORS} files have errors please check the output above for more details"
+  exit 1
+else
+  echo "🎉 All good!"
+fi
