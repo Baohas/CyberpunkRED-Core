99d594afbd844b5f96961b272bf6e743c890d21a
dsorrent <Darin.Sorrentino@gmail.com>
2022-12-24 11:52:12 -0500

    Adds a build test to ensure HBS files have an equal number of <div to </div statements. Fixes files which do not meet this criteria.

---
+++ .gitlab/pipeline_tests/test-handlebars-divs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ERRORS=0
+# Check hbs_location exits
+HBS_LOCATION="src/templates"
+
+# Check the HBS_LOCATION exists
+if [[ ! -d "${HBS_LOCATION}" ]]; then
+  echo "❌ Unable to find ${HBS_LOCATION}"
+  exit 1
+fi
+
+ALL_FILES=$(find "${HBS_LOCATION}" -type f -print)
+
+if [[ -z "${ALL_FILES}" ]]; then
+  echo "❌ Unable to find any template files in ${HBS_LOCATION}"
+  exit 1
+fi
+
+for FILE in ${ALL_FILES}; do
+  OPEN_DIV=$(grep -Eo '<div' ${FILE} | wc -l | awk '{print $1}')
+  CLOSE_DIV=$(grep -Eo '</div' ${FILE} | wc -l | awk '{print $1}')
+  if [[ ${OPEN_DIV} -ne ${CLOSE_DIV} ]]; then
+    echo "❌ Mismatch of '<div' (${OPEN_DIV}) and '</div' (${CLOSE_DIV}) in ${FILE}"
+    ERRORS=$(( ${ERRORS} + 1 ))
+  fi
+done
+
+# If some trace messages are missing or incorrect fail this job
+if [[ "${ERRORS}" -gt 0 ]]; then
+  echo "❌ There are ${ERRORS} hbs files with mismatched <div and </div statements.
+  echo "Each HBS file should have an opened '<div' and a matching closing '</div' statement".
+  echo "Please add or correct this issue."
+  exit 1
+else
+  echo "🎉 All good!"
+fi
