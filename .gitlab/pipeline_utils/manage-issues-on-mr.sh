c295cff4cbc12c442decabf11239ea838e4b2248
Ryan Walder <ryan@tfu.io>
2023-01-04 12:16:06 +0000

    Add 'manage-issues-on-mr' ci job

--- .gitlab/pipeline_utils/manage-issues-on-mr.sh
+++ .gitlab/pipeline_utils/manage-issues-on-mr.sh
@@ -3,26 +3,15 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # Variables that are set by GitLab CI environment
-# CI_API_V4_URL, CI_PROJECT_ID, CHOOM_BOT_API
+# CI_API_V4_URL, CI_PROJECT_ID, CI_MERGE_REQUEST_IID, CHOOM_BOT_API
 
 # URL to use as the base for out API calls
 PROJECT_URL="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}"
 
-# This is insane, GitLab doesn't pass the MR IID to a 'push' event in any
-# ENVARS as you would expect but it does put spmething along the lines of
-# 'See merge request cyberpunk-red-team/ci-testing!5' in `env`?! so we can
-# grep for this and extract the MR IID.
-MR_IID=$(
-  env |
-    grep 'See merge request' |
-    grep -Eom 1 '[0-9]{1,10}' || echo ""
-)
+# Get the MR_IID from the environment
+MR_IID="${CI_MERGE_REQUEST_IID:-''}"
 
-# However if the source of the pipeline is 'push' this also applies to direct
-# pushes to the branch, so they won't have the text so we need to set ISSUES
-# to an empty array so we can skip the labelling if it's just a standard push
-# to `dev` not from an MR.
-
+# If we have a MR_IID get a list of issues mentioned in the MR
 if [[ -n ${MR_IID} ]]; then
   mapfile -t ISSUES < <(
     curl \
@@ -41,22 +30,14 @@ fi
 # Labels to add to the Issues in ISSUES
 # Case sensitive
 LABELS_TO_ADD=(
-  "Workflow::In Dev"
-  "Test Me!"
+  "Workflow::Dev Merge"
 )
 
-# Some issues we just want to close once merged like build system changes
-# Case sensitive
-LABELS_TO_CLOSE=(
-  "Type::Build System"
+# If we havent changed the Bug scoped variable change it to 'Bug::Confirmed'
+BUG_LABELS_TO_CHANGE=(
+  "Bug::Unconfirmed"
+  "Bug::New"
 )
-
-# Note to add to each issue mentioned in the MR
-NOTE="We have just merged !${MR_IID} into \`dev\`.
-
-This means it's on track to be in the next release but it needs testing first.
-
-If you want to help test this please check out the documentation on [Development Release](https://gitlab.com/cyberpunk-red-team/fvtt-cyberpunk-red-core/-/wikis/releases/Development-Releases) and how to install them."
 
 # Run checks on an issue
 # $1 == issue_id
@@ -88,17 +69,6 @@ function check_issue() {
   fi
 }
 
-# Close the issue
-# $1 == issue_id
-function close_issue() {
-  curl \
-    --data-urlencode "state_event=close" \
-    --request PUT \
-    --silent \
-    --header "PRIVATE-TOKEN: ${CHOOM_BOT_API}" \
-    "${PROJECT_URL}/issues/$1" >/dev/null
-}
-
 # Update the Labels using LABELS_TO_ADD defined above
 # $1 == issue_id
 function add_labels() {
@@ -113,15 +83,13 @@ function add_labels() {
     "${PROJECT_URL}/issues/$1" >/dev/null
 }
 
-# Leave a Note (comment) on the issue to say it's in dev and needs testing
-# $1 == issue_id
-function add_note() {
+function change_labels() {
   curl \
-    --data-urlencode "body=${NOTE}" \
-    --request POST \
+    --data-urlencode "add_labels=Bug::Confirmed" \
+    --request PUT \
     --silent \
     --header "PRIVATE-TOKEN: ${CHOOM_BOT_API}" \
-    "${PROJECT_URL}/issues/$1/notes" >/dev/null
+    "${PROJECT_URL}/issues/$1" >/dev/null
 }
 
 function main() {
@@ -138,23 +106,21 @@ function main() {
           jq --raw-output '.labels[]'
       )
 
-      # Check if the issue's lables are in our LABELS_TO_CLOSE array
-      close=0
+      # Check if the issue's lables are in our BUG_LABELS_TO_CHANGE array
+      change=0
       for label in "${labels[@]}"; do
-        for ltc in "${LABELS_TO_CLOSE[@]}"; do
+        for ltc in "${BUG_LABELS_TO_CHANGE[@]}"; do
           if [[ "${ltc}" == "${label}" ]]; then
-            # If there is a match mark the issue to be closed
-            ((close = close + 1))
+            # If there is a match mark the issue to be changed
+            ((change = change + 1))
           fi
         done
       done
-      # Close if in LABELS_TO_CLOSE
-      if [[ ${close} -gt 0 ]]; then
-        close_issue "${issue}"
-      else # Add the labels from LABELS_TO_ADD and add NOTE to the issue
-        add_labels "${issue}"
-        add_note "${issue}"
+      # Close if in BUG_LABELS_TO_CHANGE
+      if [[ ${change} -gt 0 ]]; then
+        change_labels "${issue}"
       fi
+      add_labels "${issue}"
     fi
   done
 }
