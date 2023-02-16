16cacedae443b87a4b1cc476eabd5864bdffa0fe
Ryan Walder <ryan@tfu.io>
2023-02-16 15:27:58 +0000

    Refactor fight tab / armor

--- src/modules/system/preload-templates.js
+++ src/modules/system/preload-templates.js
@@ -25,6 +25,7 @@ export default function preloadHandlebarsTemplates() {
     `systems/${game.system.id}/templates/actor/character/bottom-pane/cpr-lifepath-tab.hbs`,
     `systems/${game.system.id}/templates/actor/character/bottom-pane/cpr-role-tab.hbs`,
     `systems/${game.system.id}/templates/actor/character/bottom-pane/fight/cpr-weapons-list.hbs`,
+    `systems/${game.system.id}/templates/actor/character/bottom-pane/fight/cpr-armor-list.hbs`,
 
     // Actor: Character - Skill Tab
     `systems/${game.system.id}/templates/actor/character/right-pane/tabs/skills/cpr-skills-category.hbs`,
