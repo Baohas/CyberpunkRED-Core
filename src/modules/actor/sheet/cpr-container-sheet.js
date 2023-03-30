7117f3f7076ff67d518efc5f7b737116c8a0b5d2
Ryan Walder <ryan@tfu.io>
2023-03-30 18:24:33 +0100

    Update Container Sheet for CSS theming

--- src/modules/actor/sheet/cpr-container-sheet.js
+++ src/modules/actor/sheet/cpr-container-sheet.js
@@ -24,6 +24,7 @@ export default class CPRContainerActorSheet extends CPRActorSheet {
     LOGGER.trace("defaultOptions | CPRContainerActorSheet | Called.");
     return mergeObject(super.defaultOptions, {
       template: `systems/${game.system.id}/templates/actor/cpr-container-sheet.hbs`,
+      width: 990,
     });
   }
 
