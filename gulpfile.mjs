7c06adb2a773d9f66bdc0ceb2983549d52db356a
Ryan Walder <ryan@tfu.io>
2023-04-21 15:22:00 +0100

    Add generateBabele to exportPacks gulp target

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -18,7 +18,7 @@ export const assets = gulp.parallel(
 );
 
 // Export packs from Foundry to src/packs
-export const extractPacks = gulp.series(packs.extPacks);
+export const extractPacks = gulp.series(packs.extPacks, packs.genPacksBabele);
 
 // Generate the Babele translation files from the YAML fragments
 export const generateBabele = gulp.series(packs.genPacksBabele);
