198e61273603f284b1c87ce85a2bab72d5ecdf96
jalensailin <jalen.michalslevy@gmail.com>
2023-05-20 14:30:00 -0400

    initial migration changes in

---
+++ src/modules/system/migrate/scripts/010-ammoOverride.js
@@ -0,0 +1,79 @@
+/* eslint-disable foundry-cpr/logger-after-function-definition */
+
+import CPRMigration from "../cpr-migration.js";
+import CPRSystemUtils from "../../../utils/cpr-systemUtils.js";
+import LOGGER from "../../../utils/cpr-logger.js";
+
+export default class AmmoOverrideMigration extends CPRMigration {
+  constructor() {
+    LOGGER.trace("constructor | Source Migration");
+    super();
+    this.version = 10;
+    this.name = "Ammo Override Migration";
+  }
+
+  /**
+   * Executed before the migration takes place, see run() in the base migration class.
+   */
+  async preMigrate() {
+    LOGGER.trace(`preMigrate | ${this.version}-${this.name}`);
+    CPRSystemUtils.DisplayMessage(
+      "notify",
+      CPRSystemUtils.Localize("CPR.migration.effects.beginMigration")
+    );
+    LOGGER.log(`Starting migration: ${this.name}`);
+  }
+
+  /**
+   * Takes place after the data migration completes.
+   */
+  async postMigrate() {
+    LOGGER.trace(`postMigrate | ${this.version}-${this.name}`);
+    LOGGER.log(`Finishing migration: ${this.name}`);
+  }
+
+  /**
+   * Here's the real work.
+   *
+   * @param {CPRItem} item
+   */
+  static async migrateItem(item) {
+    LOGGER.trace(`migrateItem | ${this.version}-${this.name}`);
+    const updateData = item.isOwned ? { _id: item._id } : {};
+
+    if (item.type === "ammo") {
+      const overrides = {
+        damage: {
+          mode: "none",
+          value: "3d6",
+          minimum: "1d6",
+        },
+        autofire: {
+          mode: "none",
+          value: -1,
+          minimum: 3,
+        },
+      };
+      updateData["system.overrides"] = overrides;
+      return item.isOwned ? updateData : item.update(updateData);
+    }
+
+    return null;
+  }
+
+  /**
+   * Simply make sure owned items are updated too.
+   *
+   * @param {CPRActor} actor
+   */
+  async migrateActor(actor) {
+    LOGGER.trace(`migrateActor | ${this.version}-${this.name}`);
+    const itemUpdates = [];
+    for (const item of actor.items) {
+      // eslint-disable-next-line no-await-in-loop
+      const updateData = await AmmoOverrideMigration.migrateItem(item);
+      if (updateData !== null) itemUpdates.push(updateData);
+    }
+    return actor.updateEmbeddedDocuments("Item", itemUpdates);
+  }
+}
