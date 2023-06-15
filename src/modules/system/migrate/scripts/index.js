177f4b5435328ce48d08680562e5ddd0da5390c0
dsorrent <Darin.Sorrentino@gmail.com>
2023-06-14 23:06:50 -0700

    Fixing the abilities issue. #812

--- src/modules/system/migrate/scripts/index.js
+++ src/modules/system/migrate/scripts/index.js
@@ -16,3 +16,4 @@ export { default as ItemSmartGlassesMigration } from "./009-fixSmartGlasses.js";
 export { default as AmmoOverrideMigration } from "./010-ammoOverride.js";
 export { default as ArmorPiercingMigration } from "./011-armorPiercing.js";
 export { default as InstalledItemMigrationFix } from "./012-installedItemMigrationFix.js";
+export { default as RoleAbilitiesFix } from "./013-roleAbilitiesFix.js";
