c0ec9d7d02e6831fece3d3d9cd42fd20f373ed7c
jalensailin <jalen.michalslevy@gmail.com>
2023-07-21 17:22:29 -0400

    migration script written

--- src/modules/system/migrate/scripts/index.js
+++ src/modules/system/migrate/scripts/index.js
@@ -22,3 +22,4 @@ export { default as aeFix } from "./015-aeFix.js";
 export { default as dvCompendiumMigration } from "./016-dvCompendium.js";
 export { default as criticalInjuryCompendiumMigration } from "./017-critTables.js";
 export { default as CustomSkillFix } from "./018-customSkillFix.js";
+export { default as ImportedActorFix } from "./019-importedActorFix.js";
