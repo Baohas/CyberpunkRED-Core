3e785ae820e46948a832f2d4b4273e4eb84cb759
jalensailin <jalen.michalslevy@gmail.com>
2023-08-18 15:33:24 -0400

    update prototype token default images to match

--- src/modules/hooks/actor.js
+++ src/modules/hooks/actor.js
@@ -25,7 +25,10 @@ const actorHooks = () => {
     LOGGER.trace("preCreateActor | actorHooks | Called.");
     if (typeof createData.img === "undefined") {
       const actorImage = SystemUtils.GetDefaultImage("Actor", createData.type);
-      doc.updateSource({ img: actorImage });
+      doc.updateSource({
+        img: actorImage,
+        "prototypeToken.texture.src": actorImage,
+      });
     }
   });
 
