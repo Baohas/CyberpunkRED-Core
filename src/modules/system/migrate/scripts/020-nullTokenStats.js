a405fc96ea1d06727faa32cee0fe3fe17cb3a5e4
jalensailin <jalen.michalslevy@gmail.com>
2023-08-21 13:04:06 -0400

    migration runs on every item, not just unlinked

--- src/modules/system/migrate/scripts/020-nullTokenStats.js
+++ src/modules/system/migrate/scripts/020-nullTokenStats.js
@@ -34,34 +34,41 @@ export default class NullStatsMigration extends CPRMigration {
   }
 
   /**
-   * Black Ice have two note sections that could easily be combined into one.
-   * This will combine the two note sections.
+   * We have faced a very strange issue where some items have null `item._stats`
+   * fields on unlinked tokens. This was not an issue in v10 but it seems that
+   * v11 doesn't like it. After migration to v11 and then a refresh of the page,
+   * these items disappear. Luckily, we can manually update the _stats field
+   * and that is what this function does.
+   *
+   * @param {CPRItem} item
+   */
+  static async migrateItem(item) {
+    LOGGER.trace(`migrateItem | ${this.version}-${this.name}`);
+    if (item._stats && Object.values(item._stats).some((stat) => !stat)) {
+      return item.update({
+        _stats: {
+          coreVersion: "10.303", // Doesn't matter
+          createdTime: 1692463494866, // This is just a unix timestamp
+          lastModifiedBy: "CPRCMigration088", // This can be anything [A-Za-z0-9]{16}
+          modifiedTime: 1692463494866, // This is just a unix timestamp
+          systemId: game.system.id, // This needs to be this obviously
+          systemVersion: "v0.87.6", // Doesn't matter
+        },
+      });
+    }
+    return Promise.resolve();
+  }
+
+  /**
+   * Migrate each owned item.
    *
    * @param {CPRActor} actor
    */
   async migrateActor(actor) {
     LOGGER.trace(`migrateActor | ${this.version}-${this.name}`);
-    // Return if not Black Ice.
-    if (actor.token && !actor.token.actorLink) {
-      const nullStatItems = actor.items.filter((i) => {
-        return Object.values(i._stats).some((stat) => !stat);
-      });
-      const nullStatUpdates = [];
-      nullStatItems.forEach((i) => {
-        nullStatUpdates.push({
-          _id: i.id,
-          _stats: {
-            coreVersion: "10.303",
-            createdTime: 1692463494866,
-            lastModifiedBy: "Tk20S39ggUnMuKpt",
-            modifiedTime: 1692463494866,
-            systemId: game.system.id,
-            systemVersion: "v0.87.6",
-          },
-        });
-      });
-      return actor.updateEmbeddedDocuments("Item", nullStatUpdates);
+    for (const item of actor.items) {
+      // eslint-disable-next-line no-await-in-loop
+      await NullStatsMigration.migrateItem(item);
     }
-    return Promise.resolve();
   }
 }
