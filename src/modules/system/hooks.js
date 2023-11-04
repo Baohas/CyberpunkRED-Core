354c5422b253719c180459ad9ce37c3955f7181e
Ryan Walder <ryan@tfu.io>
2023-11-04 13:23:24 +0000

    Sort hooks.js alphabetically

--- src/modules/system/hooks.js
+++ src/modules/system/hooks.js
@@ -1,25 +1,25 @@
 import * as actorHooks from "../hooks/actor.js";
-import * as tokenHooks from "../hooks/token.js";
 import * as actorSheetHooks from "../hooks/actor-sheet.js";
 import * as chatHooks from "../hooks/chat.js";
-import * as itemHooks from "../hooks/item.js";
-import * as tokenHudHooks from "../hooks/tokenhud.js";
-import * as uiHooks from "../hooks/ui.js";
-import * as hotbarHooks from "../hooks/hotbar.js";
 import * as externalHooks from "../hooks/external-modules.js";
 import * as folderHooks from "../hooks/folder.js";
+import * as hotbarHooks from "../hooks/hotbar.js";
+import * as itemHooks from "../hooks/item.js";
 import * as renderItemDirHooks from "../hooks/render-item-directory.js";
+import * as tokenHooks from "../hooks/token.js";
+import * as tokenHudHooks from "../hooks/tokenhud.js";
+import * as uiHooks from "../hooks/ui.js";
 
 export default function registerHooks() {
   actorHooks.default();
   actorSheetHooks.default();
   chatHooks.default();
+  externalHooks.default();
   folderHooks.default();
+  hotbarHooks.default();
   itemHooks.default();
+  renderItemDirHooks.default();
+  tokenHooks.default();
   tokenHudHooks.default();
   uiHooks.default();
-  hotbarHooks.default();
-  tokenHooks.default();
-  externalHooks.default();
-  renderItemDirHooks.default();
 }
