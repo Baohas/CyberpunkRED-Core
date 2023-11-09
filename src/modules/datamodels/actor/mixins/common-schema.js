12c1d2cdaf46d78089f639c8cc5262325493b86b
Ryan Walder <ryan@tfu.io>
2023-11-09 19:40:35 +0000

    Fix ordering of STATs

--- src/modules/datamodels/actor/mixins/common-schema.js
+++ src/modules/datamodels/actor/mixins/common-schema.js
@@ -12,17 +12,23 @@ export default class CommonSchema extends foundry.abstract.DataModel {
     const { fields } = foundry.data;
     const hasMax = true;
     return {
+      /**
+       *  !IMPORTANT!
+       *
+       *  Do not alphabetise these, we need them in this order to order them
+       *  correctly on the character sheet.
+       */
       stats: new fields.SchemaField({
-        body: new fields.SchemaField(StatSchema.defineSchema()),
-        cool: new fields.SchemaField(StatSchema.defineSchema()),
-        dex: new fields.SchemaField(StatSchema.defineSchema()),
-        emp: new fields.SchemaField(StatSchema.defineSchema(hasMax, -10)),
         int: new fields.SchemaField(StatSchema.defineSchema()),
+        ref: new fields.SchemaField(StatSchema.defineSchema()),
+        dex: new fields.SchemaField(StatSchema.defineSchema()),
+        tech: new fields.SchemaField(StatSchema.defineSchema()),
+        cool: new fields.SchemaField(StatSchema.defineSchema()),
+        will: new fields.SchemaField(StatSchema.defineSchema()),
         luck: new fields.SchemaField(StatSchema.defineSchema(hasMax)),
         move: new fields.SchemaField(StatSchema.defineSchema()),
-        ref: new fields.SchemaField(StatSchema.defineSchema()),
-        tech: new fields.SchemaField(StatSchema.defineSchema()),
-        will: new fields.SchemaField(StatSchema.defineSchema()),
+        body: new fields.SchemaField(StatSchema.defineSchema()),
+        emp: new fields.SchemaField(StatSchema.defineSchema(hasMax, -10)),
       }),
       externalData: new fields.SchemaField({
         currentArmorBody: new fields.SchemaField(
