6d428b0c2fe6247ee2559aaa0786585d55af7d65
Ryan Walder <ryan@tfu.io>
2023-12-04 17:45:02 +0000

    Split hooks/item-sheet.js

--- src/modules/hooks/actor/sheet/resize-type-tag.js
+++ src/modules/hooks/actor/sheet/resize-type-tag.js
@@ -16,6 +16,14 @@ const ResizeTypeTag = () => {
       SheetUtils.setCssClassWidth(html, ".type-tag");
     });
   });
+  // Run on Item Sheets
+  // This should arguably be in items/sheet/resize-type-tag.js but it's
+  // probably fine here as it's exactly the same as the Actor hook.
+  Hooks.on("renderItemSheet", (_, html) => {
+    window.requestAnimationFrame(() => {
+      SheetUtils.setCssClassWidth(html, ".type-tag");
+    });
+  });
 };
 
 export default ResizeTypeTag;
