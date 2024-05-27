7884a2ba16d9bf98755acbc36145d2a9f0586420
jalensailin <jalen.michalslevy@gmail.com>
2024-05-27 18:11:48 -0400

    remove bad parameter; didn't cause issues in v11, but does in v12; I can't figure out what it was doing there in the first place

--- src/modules/chat/cpr-chat.js
+++ src/modules/chat/cpr-chat.js
@@ -84,7 +84,7 @@ export default class CPRChat {
         const alias = actor.name;
         chatOptions.speaker = { actor, alias };
       }
-      return ChatMessage.create(chatOptions, false);
+      return ChatMessage.create(chatOptions);
     });
   }
 
