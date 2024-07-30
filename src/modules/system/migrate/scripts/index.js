fc683901695ff3d506e54750b60099bedc228e0c
Ryan Walder <ryan@tfu.io>
2024-07-30 16:38:15 +0100

    Add migration script for item quality

--- src/modules/system/migrate/scripts/index.js
+++ src/modules/system/migrate/scripts/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/no-cycle */
 // add your migration scripts here
 export { default as RoleAbilityNaNMigration } from "./025-roleAbilities.js";
+export { default as ItemQualityMigration } from "./026-itemQuality.js";
