7dc3626d1456095bb94ca38f1a22070066fb826d
Ryan Walder <ryan@tfu.io>
2024-08-26 12:18:19 +0100

    Update test-choombot-key.sh test to work locally

--- .gitlab/pipeline_tests/test-choombot-key.sh
+++ .gitlab/pipeline_tests/test-choombot-key.sh
@@ -7,21 +7,27 @@ IFS=$'\n\t'
 
 echo "Testing CHOOM_BOT_API validity."
 
-# Check we have a value for CHOOM_BOT_API. If not exit with an error
-if [[ -z "${CHOOM_BOT_API}" ]]; then
-  echo "❌ CHOOM_BOT_API is blank. See the wiki for more information."
-  echo "https://gitlab.com/cyberpunk-red-team/fvtt-cyberpunk-red-core/-/wikis/ops/GitLab#bot-accounts--access-tokens"
-  exit 1
-fi
+CI=${CI:-false}
 
-# Check if CHOOM_BOT_API can access the API as expected
-# Short circuit to true so we can parse the response
-CHOOM_TEST_RESPONSE=$(
-  curl \
-    --silent \
-    --header "PRIVATE-TOKEN: ${CHOOM_BOT_API}" \
-    "${CI_API_V4_URL}/user" || true
-)
+# Only run if we're in GitLab CI
+if [[ "${CI}" == "true" ]]; then
+  # Check we have a value for CHOOM_BOT_API. If not exit with an error
+  if [[ -z "${CHOOM_BOT_API}" ]]; then
+    echo "❌ CHOOM_BOT_API is blank. See the wiki for more information."
+    echo "https://gitlab.com/cyberpunk-red-team/fvtt-cyberpunk-red-core/-/wikis/ops/GitLab#bot-accounts--access-tokens"
+    exit 1
+  fi
+  # Check if CHOOM_BOT_API can access the API as expected
+  # Short circuit to true so we can parse the response
+  CHOOM_TEST_RESPONSE=$(
+    curl \
+      --silent \
+      --header "PRIVATE-TOKEN: ${CHOOM_BOT_API}" \
+      "${CI_API_V4_URL}/user" || true
+  )
+else
+  CHOOM_TEST_RESPONSE='{"state": "active"}'
+fi
 
 # Check if the user has "state": "active" in the response. Else error out
 if [[ $(echo "${CHOOM_TEST_RESPONSE}" | jq -r '.state') == "active" ]]; then
