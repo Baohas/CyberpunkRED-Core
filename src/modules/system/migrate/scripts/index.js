655fd00513d1e3c4077536218bfb04664954262d
Ryan Walder <ryan@tfu.io>
2024-09-29 11:38:04 +0100

    Add migration script for armor.system.penalty

--- src/modules/system/migrate/scripts/index.js
+++ src/modules/system/migrate/scripts/index.js
@@ -4,3 +4,4 @@ export { default as RoleAbilityNaNMigration } from "./025-roleAbilities.js";
 export { default as ItemQualityMigration } from "./026-itemQuality.js";
 export { default as SkillTypeQualityMigration } from "./027-skillType.js";
 export { default as AttackableIgnoreArmorMigration } from "./028-ignoreArmor.js";
+export { default as ArmorPenaltyMigration } from "./029-armor-penalties.js";
