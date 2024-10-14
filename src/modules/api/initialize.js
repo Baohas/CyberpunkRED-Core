91aaf5caecb8208c5a08f9fdb722b799ba0b6b61
Ryan Walder <ryan@tfu.io>
2024-10-14 13:03:31 +0100

    Change game.apr.api to Object

--- src/modules/api/initialize.js
+++ src/modules/api/initialize.js
@@ -1,33 +1,47 @@
-async function _loadActorFunctions() {
+import empableItems from "./actor/empable-items.js";
+
+/**
+ * Loads actor functions from predefined modules and returns them as an object.
+ *
+ * @private
+ * @returns {Object} An object where keys are function names and values
+ *                   are the corresponding functions.
+ */
+function _loadActorFunctions() {
   const functions = {};
 
-  const actorModuleFiles = ["./actor/empable-items.js"];
+  // Statically added modules
+  const modules = [empableItems];
 
-  const importPromises = actorModuleFiles.map(async (filePath) => {
-    const module = await import(filePath);
-    const defaultExport = module.default;
-
-    // Infer the function name
-    if (typeof defaultExport === "function" && defaultExport.name) {
-      const functionName = defaultExport.name;
-      functions[functionName] = defaultExport;
+  modules.forEach((module) => {
+    if (typeof module === "function" && module.name) {
+      // Function's name is used as the key
+      const functionName = module.name;
+      functions[functionName] = module;
     }
   });
-
-  await Promise.all(importPromises);
 
   return functions;
 }
 
-async function initializeAPI() {
+/**
+ * Initializes the API by creating the `game.cpr.api.actor` namespace and
+ * loading actor functions.
+ *
+ * @returns {Object} The initialized `game.cpr.api` object with actor functions.
+ */
+function initializeAPI() {
   // Initialize the API root namespace
   if (!game.cpr) game.cpr = {};
   if (!game.cpr.api) game.cpr.api = {};
+
   if (!game.cpr.api.actor) {
     game.cpr.api.actor = {};
   }
-  // Load actor functions
-  const actorFunctions = await _loadActorFunctions();
+
+  // Load actor functions synchronously
+  const actorFunctions = _loadActorFunctions();
+
   // Ensure game.cpr.api.actor remains defined
   if (!game.cpr.api.actor) {
     game.cpr.api.actor = {};
@@ -35,6 +49,9 @@ async function initializeAPI() {
 
   // Assign each of the actor functions to the namespace correctly
   Object.assign(game.cpr.api.actor, actorFunctions);
+
+  // Return the initialized API object
+  return game.cpr.api;
 }
 
 export default initializeAPI;
