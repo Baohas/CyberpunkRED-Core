ac36158d67aa3d7830fe1a166ee7d38ea5e2dd11
valkeryie666 <valkeryie666@gmail.com>
2025-03-20 21:52:05 -0400

    Add system class and damage to program roll card for blackice

--- src/modules/actor/cpr-black-ice.js
+++ src/modules/actor/cpr-black-ice.js
@@ -56,6 +56,10 @@ export default class CPRBlackIceActor extends Actor {
       cprRoll.rollCardExtraArgs.program = {
         class: "blackice",
         blackIceType: this.system.class,
+        system: {
+          class: "blackice",
+          damage: "blackice",
+        },
       };
     }
     return cprRoll;
