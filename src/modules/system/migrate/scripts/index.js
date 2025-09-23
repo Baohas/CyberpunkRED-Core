c4907693edf15979dfe3bab3d76b58d5d5020912
jalensailin <jalen.michalslevy@gmail.com>
2025-09-22 21:05:20 -0400

    Move `flags.cprInstallTree` to `flags.cyberpunk-red-core` namespace

--- src/modules/system/migrate/scripts/index.js
+++ src/modules/system/migrate/scripts/index.js
@@ -9,3 +9,4 @@ export { default as ElectronicMigration } from "./030-isElectronic.js";
 export { default as NetarchTilesMigration } from "./031-netarch-tiles.js";
 export { default as AmmoIsInstallable } from "./032-ammo-is-installable.js";
 export { default as UUIDsToIds } from "./033-uuids-to-ids.js";
+export { default as CPRInstallTreeFlagMigration } from "./034-migrate-cprInstallTree-flag.js";
