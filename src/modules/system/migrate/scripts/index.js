56802c58ba6cef63e934750a80cc967b7518820d
rhlowe <rhlowe@gmail.com>
2025-10-05 13:47:05 -0500

    Fix unallowed type programs on cyberware
    Signed-off-by: rhlowe <rhlowe@gmail.com>

--- src/modules/system/migrate/scripts/index.js
+++ src/modules/system/migrate/scripts/index.js
@@ -11,3 +11,4 @@ export { default as AmmoIsInstallable } from "./032-ammo-is-installable.js";
 export { default as UUIDsToIds } from "./033-uuids-to-ids.js";
 export { default as CPRInstallTreeFlagMigration } from "./034-migrate-cprInstallTree-flag.js";
 export { default as SetActorSheetCreatedWeaponSkill } from "./035-set-weapon-skill.js";
+export { default as ReplaceProgramsWithProgram } from "./036-correct-allowed-types.js";
