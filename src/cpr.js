0a8dfb85c87bc72eb63a32dcb784039b57540399
Eolind <sunn0833@gmail.com>
2025-12-07 04:29:07 +0200

    fix DV Tables on ruler feature for v13

--- src/cpr.js
+++ src/cpr.js
@@ -275,7 +275,6 @@ Hooks.once("init", async () => {
   preloadHandlebarsTemplates();
   registerHandlebarsHelpers();
   registerSystemSettings();
-  overrideRulerFunctions();
 
   // This MUST occur after templates, helpers, and system settings are loaded or registered.
   MigrationRunner.instantiate();
@@ -290,6 +289,10 @@ Hooks.once("init", async () => {
  * but then we moved to integers for maintainability's sake.
  */
 Hooks.once("ready", async () => {
+  overrideRulerFunctions();
+  const proto = Object.getPrototypeOf(canvas.controls.ruler);
+  proto.constructor.WAYPOINT_LABEL_TEMPLATE = `systems/${game.system.id}/templates/hud/waypoint-label-dv.hbs`;
+
   if (!game.user.isGM) return;
   const { settings } = game;
 
