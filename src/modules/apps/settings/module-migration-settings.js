60dc067ad563fa98a1ea13de180cb60c2294441d
Ryan Walder <ryan@tfu.io>
2026-06-30 12:08:10 +0100

    Migrate module migration settings menu to ApplicationV2

--- src/modules/apps/settings/module-migration-settings.js
+++ src/modules/apps/settings/module-migration-settings.js
@@ -1,40 +1,64 @@
-/* eslint-disable class-methods-use-this */
+import CPR from "../../system/config.js";
 import SystemUtils from "../../utils/cpr-systemUtils.js";
 
-export default class ModuleMigrationSettings extends FormApplication {
-  /**
-   * set up default things like the html template and window size
-   *
-   * @override
-   * @static
-   */
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      title: SystemUtils.Localize("CPR.settings.moduleMigrationMenu.name"),
-      id: "module-migration-config",
-      template: `systems/${game.system.id}/templates/apps/settings/module-migration-settings.hbs`,
+const { ApplicationV2, HandlebarsApplicationMixin } = foundry.applications.api;
+
+/**
+ * Settings submenu (registered via game.settings.registerMenu) that lets a GM pick
+ * which modules' compendia should be migrated by default. The available modules are
+ * derived from game.modules at open time, so the options are built dynamically.
+ */
+export default class ModuleMigrationSettings extends HandlebarsApplicationMixin(
+  ApplicationV2,
+) {
+  /** @inheritDoc */
+  static DEFAULT_OPTIONS = {
+    id: "module-migration-config",
+    tag: "form",
+    position: {
       width: "auto",
       height: "auto",
+    },
+    window: {
+      icon: "fa-solid fa-diagram-next",
+      title: "CPR.settings.moduleMigrationMenu.name",
+      resizable: false,
+    },
+    form: {
+      handler: ModuleMigrationSettings.#onSubmit,
       closeOnSubmit: true,
-    });
+    },
+  };
+
+  /** @inheritDoc */
+  static PARTS = {
+    form: {
+      template: `systems/${CPR.systemId}/templates/apps/settings/module-migration-settings.hbs`,
+    },
+  };
+
+  get title() {
+    return `${game.system.title}: ${SystemUtils.Localize(
+      this.options.window.title,
+    )}`;
   }
 
   /**
-   * When this application (read: form window) is launched, create the data object that is
-   * consumed by the handle bars template to present options to the user.
+   * Build the data consumed by the template: the set of modules that ship relevant
+   * compendia, and which of them are currently selected for default migration.
    *
+   * @async
    * @override
    * @returns {Object}
    */
-  async getData() {
-    const data = await super.getData();
-
+  // eslint-disable-next-line class-methods-use-this
+  async _prepareContext() {
     const moduleIdSet = new Set(
       game.settings.get(game.system.id, "moduleMigrationIds"),
     );
-    data.selectedMods = Array.from(moduleIdSet);
+    const selectedMods = Array.from(moduleIdSet);
 
-    data.modules = {};
+    const modules = {};
     const compendiaTypes = ["Actor", "Item", "Scene"];
 
     // Gather modules with relevant compendia.
@@ -43,25 +67,25 @@ export default class ModuleMigrationSettings extends FormApplication {
         compendiaTypes.includes(p.type),
       );
       if (filteredPacks.size === 0) return;
-      data.modules[module.id] = module.title;
+      modules[module.id] = module.title;
     });
 
-    return data;
+    return { selectedMods, modules };
   }
 
   /**
-   * Called when the sub menu application (this thing) is submitted. Responsible for updating
-   * internal settings with what the user chose.
+   * Persist the chosen module ids. Read the checked boxes straight from the form so
+   * the result is the list of selected module ids regardless of FormDataExtended's
+   * checkbox coercion.
    *
-   * @async
-   * @override
-   * @param {*} event (not used here)
-   * @param {Object} formData - object containing module ids corresponding to choices
+   * @this {ModuleMigrationSettings}
+   * @param {SubmitEvent} _event
+   * @param {HTMLFormElement} form
    */
-  async _updateObject(event, formData) {
-    // Cast to an array if not.
-    if (!Array.isArray(formData.modIds)) formData.modIds = [formData.modIds];
-    const modIdList = formData.modIds.filter((id) => id); // remove null values
+  static async #onSubmit(_event, form) {
+    const modIdList = Array.from(
+      form.querySelectorAll('input[name="modIds"]:checked'),
+    ).map((input) => input.value);
     await game.settings.set(game.system.id, "moduleMigrationIds", modIdList);
   }
 }
