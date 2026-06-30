95117e092cd125ba85cf2d1f6751fed1e9602f68
Ryan Walder <ryan@tfu.io>
2026-06-30 12:08:10 +0100

    Remove AppV1 sheet registration and obsolete resize setting

--- src/modules/system/settings.js
+++ src/modules/system/settings.js
@@ -161,20 +161,6 @@ const registerSystemSettings = () => {
     },
   });
 
-  // Allow users to resize the CPRSheets, or not
-  game.settings.register(game.system.id, "resizeCPRSheets", {
-    name: "CPR.settings.resizeCPRSheets.name",
-    hint: "CPR.settings.resizeCPRSheets.hint",
-    scope: "client",
-    config: true,
-    type: Boolean,
-    default: true,
-    requiresReload: true,
-    onChange: (value) => {
-      LOGGER.log(`Changed resizeCPRSheets to ${value}`);
-    },
-  });
-
   // Mook Sheet Skill display method
   // Options:
   //   Level: Points
